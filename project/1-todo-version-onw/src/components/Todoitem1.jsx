bf4a1f88a876c787e3079a46dd00684b77e6cc1d
bishal454 <166239531+bishal454@users.noreply.github.com>
2025-02-20 11:38:33 +0000

    todo  app

--- project/1-todo-version-onw/src/components/Todoitem1.jsx
+++ project/1-todo-version-onw/src/components/Todoitem1.jsx
@@ -8,7 +8,8 @@ function Todoitem1(){
         <div class="col-6">{todoName}</div>
         <div class="col-4">{todoDate}</div>
         <div class="col-2">
-         <button type="button" class="btn btn-danger kg-button ">Delete</button>
+         <button type="button"
+          class="btn btn-danger kg-button ">Delete</button>
         </div>
       </div>
       </div>
